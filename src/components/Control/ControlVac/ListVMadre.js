b7e89b6a18e00dec3a8a25953378166aa310e43a
xiorena <52607573+xiorena@users.noreply.github.com>
2021-10-11 20:00:00 -0500

    responsive list vac mother

--- src/components/Control/ControlVac/ListVMadre.js
+++ src/components/Control/ControlVac/ListVMadre.js
@@ -88,21 +88,21 @@ export default function ListVMadre(props){
                <ListGroup.Item className="shadow border mt-3 mb-3">
                <Container>
                <Row className="mt-2 mb-2 justify-content-center">
+                   <Col md={2} className="align-self-center">
+                       <p style={{"color": "#2D61A4", "font-size": 19}}><b>Fecha Aplicaciòn</b> <br/>{dateFormat(item.fechaAplicacion)}</p>
+                   </Col>
+                   <Col md={2} className="align-self-center">
+                       <p style={{"color": "#2D61A4", "font-size": 19}}><b>Numero Documento</b> <br/>{item.idUsuario}</p>
+                   </Col>
                    <Col md={3} className="align-self-center">
-                       <p style={{"color": "#2D61A4", "font-size": 20}}><b>Fecha Aplicaciòn</b> <br/>{dateFormat(item.fechaAplicacion)}</p>
-                   </Col>
-                   <Col md={2} className="align-self-center">
-                       <p style={{"color": "#2D61A4", "font-size": 20}}><b>Numero Documento</b> <br/>{item.idUsuario}</p>
-                   </Col>
-                   <Col md={2} className="align-self-center">
-                       <p style={{"color": "#2D61A4", "font-size": 20}}><b>Nombre Vacuna </b> <br/>{item.nombreVacuna}</p>
+                       <p style={{"color": "#2D61A4", "font-size": 19}}><b>Nombre Vacuna </b> <br/>{item.nombreVacuna}</p>
                    </Col>
                    <Col md={2} className="row justify-content-center align-self-center">
-                       <p style={{"color": "#2D61A4", "font-size": 20}}><b>Edad Gestacional </b> <br/>{item.edadGestacional}</p>
+                       <p style={{"color": "#2D61A4", "font-size": 19}}><b>Edad Gestacional </b> <br/>{item.edadGestacional}</p>
                    </Col>
                    
                    <Col md={3} className="align-self-right">
-                        <p style={{"color": "#2D61A4", "font-size": 20}}><b> Acciones </b> <br/>
+                        <p style={{"color": "#2D61A4", "font-size": 19}}><b> Acciones </b> <br/>
                             <Link to={`/admin/EditControlVacMadre/${item.id}/${documento}`} className="btn btn-warning mx-0">
                                  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-pen-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg" data-tip data-for = "boton3" >
                                      <path fill-rule="evenodd" d="M13.498.795l.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
